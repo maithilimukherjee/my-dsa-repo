f2896bf34db32bed77725abfdd07caf4a6f87097
Maithili Mukherjee <86041684+maithilimukherjee@users.noreply.github.com>
2025-07-12 19:32:02 +0530

    Update twosum.py

--- twosum.py
+++ twosum.py
@@ -1,8 +1,25 @@
-class Solution(object):
+# LC 1 - Two Sum
+# https://leetcode.com/problems/two-sum/
+
+# ----------------------------------------
+# 🐌 Brute-force solution (O(n^2))
+# ----------------------------------------
+
+class SolutionBrute(object):
     def twoSum(self, nums, target):
         for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
+            for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
-                    return i,j
-        
-        
+                    return i, j
+
+# ----------------------------------------
+# ⚡ Optimized solution (O(n)) using hashmap
+# ----------------------------------------
+
+def twoSum(nums, target):
+    hashmap = {}  # value -> index
+    for i, num in enumerate(nums):
+        diff = target - num
+        if diff in hashmap:
+            return [hashmap[diff], i]
+        hashmap[num] = i
