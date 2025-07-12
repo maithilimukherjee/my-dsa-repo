02d8f5d041eed94d665339a25fff283cb8c46298
Maithili Mukherjee <86041684+maithilimukherjee@users.noreply.github.com>
2025-07-12 19:58:34 +0530

    Create simple-sum.py

---
+++ simple-sum.py
@@ -0,0 +1,14 @@
+# LC 2235 - Add Two Integers
+# https://leetcode.com/problems/add-two-integers/
+# Time: O(1), Space: O(1)
+
+class Solution(object):
+    def sum(self, num1, num2):
+        """
+        Returns the sum of two integers.
+
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+        return num1 + num2
