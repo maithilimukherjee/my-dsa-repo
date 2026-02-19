c11a82c7fb7c0c306d8657195628ea1b1175c64a
maithili <86041684+maithilimukherjee@users.noreply.github.com>
2026-02-19 22:36:27 +0530

    Implement rowWithMax1s method in Solution class

---
+++ rowMax1s.py
@@ -0,0 +1,18 @@
+class Solution:
+    def rowWithMax1s(self, arr):
+        
+        if not arr:
+            return -1
+        
+        n = len(arr)
+        m = len(arr[0])
+        
+        j = m - 1
+        ans = -1
+        
+        for i in range(n):
+            while j >= 0 and arr[i][j] == 1:
+                j -= 1
+                ans = i
+        
+        return ans
