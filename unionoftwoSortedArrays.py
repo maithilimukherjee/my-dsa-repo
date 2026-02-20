20dd13c46fc39b91e96d0ac88d2dbdc665763083
maithili <86041684+maithilimukherjee@users.noreply.github.com>
2026-02-21 00:21:56 +0530

    Implement findUnion method for two sorted arrays

---
+++ unionoftwoSortedArrays.py
@@ -0,0 +1,35 @@
+class Solution:
+    def findUnion(self, a, b):
+        i, j = 0, 0
+        res = []
+
+        while i < len(a) and j < len(b):
+
+            if a[i] == b[j]:
+                if not res or res[-1] != a[i]:
+                    res.append(a[i])
+                i += 1
+                j += 1
+
+            elif a[i] < b[j]:
+                if not res or res[-1] != a[i]:
+                    res.append(a[i])
+                i += 1
+
+            else:
+                if not res or res[-1] != b[j]:
+                    res.append(b[j])
+                j += 1
+
+        # leftover elements
+        while i < len(a):
+            if not res or res[-1] != a[i]:
+                res.append(a[i])
+            i += 1
+
+        while j < len(b):
+            if not res or res[-1] != b[j]:
+                res.append(b[j])
+            j += 1
+
+        return res
