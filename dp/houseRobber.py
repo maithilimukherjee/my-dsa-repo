6024b05c87503a826cf3886364921bbf24adad34
maithili <86041684+maithilimukherjee@users.noreply.github.com>
2026-02-28 13:09:45 +0530

    Add houseRobber.py with max sum calculation
    Implement dynamic programming solution for house robber problem.

---
+++ dp/houseRobber.py
@@ -0,0 +1,24 @@
+class Solution:  
+    def findMaxSum(self, arr):
+        # code here
+        
+        n=len(arr)
+        
+        if n==0:
+            return 0
+        
+        if n==1:
+            return arr[0]
+        
+        dp=n*[0]
+        
+        #base cases
+        
+        dp[0]=arr[0]
+        dp[1]=max(arr[0],arr[1])
+        
+        for i in range(2,n):
+            
+            dp[i]=max(dp[i-1],arr[i]+dp[i-2])
+        
+        return dp[n-1]
