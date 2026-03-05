0dadb7bfe31b3d73d3e882651cf3d53b2e76205c
maithili <86041684+maithilimukherjee@users.noreply.github.com>
2026-03-05 23:18:47 +0530

    Add linked list creation and print functionality
    Implemented linked list creation and printing functions.

---
+++ java_version/linked_lists/basic1.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class Main {
+
+    // function to create linked list from array
+    public static ListNode createList(int[] arr) {
+
+        if (arr.length == 0) return null;
+
+        ListNode head = new ListNode(arr[0]);
+        ListNode curr = head;
+
+        for (int i = 1; i < arr.length; i++) {
+            curr.next = new ListNode(arr[i]);
+            curr = curr.next;
+        }
+
+        return head;
+    }
+
+    // function to print linked list
+    public static void printList(ListNode head) {
+
+        ListNode temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.val + " -> ");
+            temp = temp.next;
+        }
+
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5};
+
+        ListNode head = createList(arr);
+
+        printList(head);
+    }
+}
