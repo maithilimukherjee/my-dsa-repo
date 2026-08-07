c3abece9eadd81055d6c44290f81f7b293e63fc3
maithili <86041684+maithilimukherjee@users.noreply.github.com>
2026-08-07 13:39:11 +0530

    Add solution for finding shortest subarray degree
    Implement a solution to find the length of the shortest subarray with the same degree as the input array.

---
+++ subarrays/degreeArray.py
@@ -0,0 +1,31 @@
+#lc 697
+class Solution(object):
+    def findShortestSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        first = {}
+        last = {}
+        count = {}
+
+        for i, num in enumerate(nums):
+
+            if num not in first:
+                first[num] = i
+            last[num] = i
+            count[num] = count.get(num,0) + 1
+        
+        degree = max(count.values())
+        min_length = len(nums)
+
+        for num in count:
+
+            if count[num]==degree:
+                min_length = min(min_length, last[num]-first[num]+1)
+
+        return min_length
+
+
+        
